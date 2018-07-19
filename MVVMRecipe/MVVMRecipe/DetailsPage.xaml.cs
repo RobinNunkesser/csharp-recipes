a5e0975910b2c6104cbc4dd537915f50c95a34e8
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-07-19 16:11:12 +0200

    New recipe: MVVMRecipe

---
+++ MVVMRecipe/MVVMRecipe/DetailsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MVVMRecipe
+{
+    public partial class DetailsPage : ContentPage
+    {
+        private DetailsViewModel detailsViewModel = new DetailsViewModel();
+
+        public DetailsPage()
+        {
+            InitializeComponent();
+            BindingContext = detailsViewModel;
+        }
+
+        public void OnResetButtonClicked(object sender, EventArgs e)
+        {
+            detailsViewModel.Forename = "";
+            detailsViewModel.Surname = "";
+        }
+    }
+}
---
+++ MVVMRecipe/MVVMRecipe/DetailsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace MVVMRecipe
+{
+    public class DetailsViewModel : INotifyPropertyChanged
+    {
+        string forename, surname;
+        public string Forename
+        {
+            get
+            {
+                return forename;
+            }
+            set
+            {
+                if (forename != value)
+                {
+                    forename = value;
+                    OnPropertyChanged("Forename");
+                }
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                if (surname != value)
+                {
+                    surname = value;
+                    OnPropertyChanged("Surname");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
