e10bf4ea1800e467fac0ca1d0edb602517a549b9
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-07-24 12:53:01 +0200

    New recipe: Properties

---
+++ PropertiesRecipe/PropertiesRecipe/PropertiesRecipePage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+
+namespace PropertiesRecipe
+{
+    public partial class PropertiesRecipePage : ContentPage
+    {
+        const string ResourceId = "PropertiesRecipe.Resources";
+        private static readonly Lazy<ResourceManager> ResMgr =
+            new Lazy<ResourceManager>(
+                () => new ResourceManager(ResourceId,
+                                          typeof(PropertiesRecipePage)
+                                          .GetTypeInfo().Assembly));
+
+        public PropertiesRecipePage()
+        {
+            InitializeComponent();
+            Debug.WriteLine(ResMgr.Value.GetString("Key"));
+        }
+    }
+}
