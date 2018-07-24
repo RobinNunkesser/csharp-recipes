382ea05148f049758e000f97b9a4ef277bba5cc7
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-07-24 13:12:12 +0200

    New recipe: REST

---
+++ RESTRecipe/RESTRecipe/iTunesService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RESTRecipe
+{
+    public class iTunesService
+    {
+        const string Url = "https://itunes.apple.com/search";
+
+        private HttpClient GetClient()
+        {
+            HttpClient client = new HttpClient();
+            var media = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(media);
+            return client;
+        }
+
+        public async Task<string> GetSongs(string term)
+        {
+            HttpClient client = GetClient();
+            try
+            {
+                UriBuilder builder = new UriBuilder(Url);
+                string searchTerm = System.Net.WebUtility.UrlEncode(term);
+                builder.Query = $"entity=song&term={searchTerm}&country=de";
+                var uri = builder.ToString();
+                HttpResponseMessage httpResponse = await client.GetAsync(uri);
+                httpResponse.EnsureSuccessStatusCode();
+                return await httpResponse.Content.ReadAsStringAsync();                
+            }
+            catch (System.Exception ex)
+            {
+                return ex.Message;                
+            } 
+            finally {
+                client.Dispose();
+            }
+        }
+    }
+}
