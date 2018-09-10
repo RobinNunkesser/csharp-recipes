4d5f1adecf3b744f77868cb34c90aca27cd12323
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-09-10 14:53:04 +0200

    New recipe: Device Sensors

---
+++ DeviceSensors/DeviceSensors/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Plugin.DeviceSensors;
+using Xamarin.Forms;
+
+namespace DeviceSensors
+{
+    public partial class MainPage : ContentPage
+    {
+        private SensorsViewModel sensorsViewModel = new SensorsViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = sensorsViewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (CrossDeviceSensors.Current.Accelerometer.IsSupported)
+            {
+                CrossDeviceSensors.Current.Accelerometer.OnReadingChanged += 
+                    (s, a) => {
+                    sensorsViewModel.Accelerometer = a.Reading.ToString();
+                    sensorsViewModel.AccValues = 
+                        new double[] {a.Reading.X, a.Reading.Y, a.Reading.Z};
+                    };
+                CrossDeviceSensors.Current.Accelerometer.StartReading();
+            }
+            if (CrossDeviceSensors.Current.Gyroscope.IsSupported)
+            {
+                CrossDeviceSensors.Current.Gyroscope.OnReadingChanged += 
+                    (s, a) => {
+                    sensorsViewModel.Gyroscope = a.Reading.ToString();
+                    sensorsViewModel.GyrValues = 
+                        new double[] { a.Reading.X, a.Reading.Y, a.Reading.Z };
+                    };
+                CrossDeviceSensors.Current.Gyroscope.StartReading();
+            }
+            if (CrossDeviceSensors.Current.Magnetometer.IsSupported)
+            {
+                CrossDeviceSensors.Current.Magnetometer.OnReadingChanged += 
+                    (s, a) => {
+                    sensorsViewModel.Magnetometer = a.Reading.ToString();
+                    sensorsViewModel.MagValues = 
+                        new double[] { a.Reading.X, a.Reading.Y, a.Reading.Z };
+                };
+                CrossDeviceSensors.Current.Magnetometer.StartReading();
+            }
+        }
+    }
+}
---
+++ DeviceSensors/DeviceSensors/SensorsViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace DeviceSensors
+{
+    public class SensorsViewModel : INotifyPropertyChanged
+    {
+        string accelerometer, gyroscope, magnetometer;
+
+        double[] accValues = new double[3];
+        double accScale = 20;
+        double accShift = 10;
+
+        double[] gyrValues = new double[3];
+        double gyrScale = 0.01;
+        double gyrShift = 0.01;
+
+        double[] magValues = new double[3];
+        double magScale = 100;
+        double magShift = 50;
+
+        public double[] AccValues
+        {
+            get => accValues.Select(x => (x + accShift) / accScale).ToArray();
+            set
+            {
+                if (accValues != value)
+                {
+                    accValues = value;
+                    OnPropertyChanged("AccValues");
+                }
+            }
+        }
+
+        public double[] GyrValues
+        {
+            get => gyrValues.Select(x => (x + gyrShift) / gyrScale).ToArray();
+            set
+            {
+                if (gyrValues != value)
+                {
+                    gyrValues = value;
+                    OnPropertyChanged("GyrValues");
+                }
+            }
+        }
+
+        public double[] MagValues
+        {
+            get => magValues.Select(x => (x + magShift) / magScale).ToArray();
+            set
+            {
+                if (magValues != value)
+                {
+                    magValues = value;
+                    OnPropertyChanged("MagValues");
+                }
+            }
+        }
+
+        public string Accelerometer
+        {
+            get => accelerometer;
+            set
+            {
+                if (accelerometer != value)
+                {
+                    accelerometer = value;
+                    OnPropertyChanged("Accelerometer");
+                }
+            }
+        }
+
+        public string Gyroscope
+        {
+            get => gyroscope;
+            set
+            {
+                if (gyroscope != value)
+                {
+                    gyroscope = value;
+                    OnPropertyChanged("Gyroscope");
+                }
+            }
+        }
+
+        public string Magnetometer
+        {
+            get => magnetometer;
+            set
+            {
+                if (magnetometer != value)
+                {
+                    magnetometer = value;
+                    OnPropertyChanged("Magnetometer");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName) {
+            var changed = PropertyChanged;
+            if (changed != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
+
+
