f3da1e17addd00ec46c0011a0dfa827366988cda
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-09-11 11:12:45 +0200

    New recipe: Barcode Scanner

---
+++ BarcodeScanner/BarcodeScanner.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace BarcodeScanner.Droid
+{
+    [Activity(Label = "BarcodeScanner",
+              Icon = "@mipmap/icon",
+              Theme = "@style/MainTheme",
+              MainLauncher = true,
+              ConfigurationChanges = 
+              ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class MainActivity : 
+        global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(
+            int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler
+                         .OnRequestPermissionsResult(requestCode,
+                                                     permissions,
+                                                     grantResults);
+        }
+    }
+}
---
+++ BarcodeScanner/BarcodeScanner/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarcodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += (result) =>
+            {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    await DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+            await Navigation.PushAsync(scanPage);
+        }
+    }
+}
