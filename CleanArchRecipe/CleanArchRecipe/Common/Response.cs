ac83100843d1ecf10a359b9fe532a652c213ca17
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-12-02 19:12:09 +0100

    Minimal class

--- CleanArchRecipe/CleanArchRecipe/Common/Response.cs
+++ CleanArchRecipe/CleanArchRecipe/Common/Response.cs
@@ -10,24 +10,6 @@ namespace CleanArchRecipe.Common
         public Response(T value) { this.value = value; tag = 1; }
         public Response(Exception error) { this.error = error; tag = 2; }
 
-        public virtual TResult Match<TResult>(
-            Func<T, TResult> successHandler,
-            Func<Exception, TResult> errorHandler
-            )
-        {
-            if (successHandler == null) 
-                throw new ArgumentNullException("successHandler");
-            if (errorHandler == null) 
-                throw new ArgumentNullException("errorHandler");
-
-            switch (tag)
-            {
-                case 1: return successHandler(value);
-                case 2: return errorHandler(error);
-                default: throw new InvalidOperationException();
-            }
-        }
-
         public virtual void Match(
             Action<T> successHandler,
             Action<Exception> errorHandler
@@ -46,67 +28,12 @@ namespace CleanArchRecipe.Common
             }
         }
 
-        public static bool operator ==(
-            Response<T> instanceA,
-            Response<T> instanceB
-            )
-        {
-            if (ReferenceEquals(instanceA, null)) return ReferenceEquals(instanceB, null);
-
-            return instanceA.Equals(instanceB);
-        }
-
-        public static bool operator !=(
-            Response<T> instanceA,
-            Response<T> instanceB)
-        {
-            return !(instanceA == instanceB);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null) return false;
-
-            var other = obj as Response<T>;
-
-            if (other == null) return false;
-
-            if (this.tag != other.tag) return false;
-
-            return Case(this).Equals(Case(other));
-        }
-
         private static object Case(Response<T> u)
         {
             switch (u.tag)
             {
                 case 1: return u.value;
                 case 2: return u.error;
-                default: throw new InvalidOperationException();
-            }
-        }
-
-        public override int GetHashCode()
-        {
-            return 17 * this.tag + Case(this).GetHashCode();
-        }
-
-        public string CaseToString()
-        {
-            switch (tag)
-            {
-                case 1: return value.ToString();
-                case 2: return error.ToString();
-                default: throw new InvalidOperationException();
-            }
-        }
-
-        public Type CaseType()
-        {
-            switch (tag)
-            {
-                case 1: return value.GetType();
-                case 2: return error.GetType();
                 default: throw new InvalidOperationException();
             }
         }
