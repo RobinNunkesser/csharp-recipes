032adb0cd757a296d7afcb3cfe8822e532eaddeb
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-05-16 16:50:53 +0200

    Added console application

---
+++ CleanArchRecipe/CleanArchRecipe.Console/MainView.cs
@@ -0,0 +1,26 @@
+﻿using BasicCleanArch;
+
+namespace CleanArchRecipe.Console
+{
+    public class MainView : IDisplayer<string>
+    {
+        private readonly IUseCase<HttpBinPostRequest, string> _interactor =
+            new HttpBinPostInteractor(new HttpBinGateway(),
+                new HttpBinResponsePresenter());
+
+        public void Display(Result<string> response)
+        {
+            response.Match(success => { System.Console.WriteLine(success); },
+                failure => { System.Console.WriteLine(failure); });
+        }
+
+        public void Start()
+        {
+            System.Console.WriteLine("Term to post? ");
+            var term = System.Console.ReadLine();
+            _interactor.Execute(new HttpBinPostRequest {term = term}, this);
+            System.Console.WriteLine("\nAsync operation running. Press key to abort");
+            var key = System.Console.ReadKey();
+        }
+    }
+}
---
+++ CleanArchRecipe/CleanArchRecipe.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchRecipe.Console
+{
+    class Program
+    {
+        private static void Main(string[] args) => new MainView().Start();
+    }
+}
