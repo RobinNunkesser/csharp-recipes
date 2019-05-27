bde7960c8365cc96611205427b927f8103cff964
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-05-27 08:01:41 +0200

    New Recipe: MapsRecipe

---
+++ MapsRecipe/MapsRecipe/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MapsRecipe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
