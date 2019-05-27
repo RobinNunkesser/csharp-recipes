af2ea203131bd7958dea02f60674ee3b6b7fd2fc
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-05-27 07:57:28 +0200

    New recipe: Geolocation

---
+++ GeolocationRecipe/GeolocationRecipe/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using Plugin.Geolocator;
+using Xamarin.Forms;
+
+namespace GeolocationRecipe
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var locator = CrossGeolocator.Current;
+
+            var position =
+                await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+            Debug.WriteLine("Position Status: {0}", position.Timestamp);
+            Debug.WriteLine("Position Latitude: {0}", position.Latitude);
+            Debug.WriteLine("Position Longitude: {0}", position.Longitude);
+        }
+    }
+}
