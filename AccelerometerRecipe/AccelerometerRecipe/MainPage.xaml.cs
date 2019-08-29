e5102837fbd0e12b4b2a6bd3687e8c5208b28986
Robin Nunkesser <robin.nunkesser@hshl.de>
2019-08-29 23:48:09 +0200

    New recipe: Accelerometer

---
+++ AccelerometerRecipe/AccelerometerRecipe/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AccelerometerRecipe
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.UI;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            // Register for reading changes, be sure to unsubscribe when finished
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+        }
+
+        void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+            Console.WriteLine($"Reading: X: {data.Acceleration.X}, Y: {data.Acceleration.Y}, Z: {data.Acceleration.Z}");
+            // Process Acceleration X, Y, and Z
+        }
+
+        public void ToggleAccelerometer(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Accelerometer.IsMonitoring)
+                    Accelerometer.Stop();
+                else
+                    Accelerometer.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
