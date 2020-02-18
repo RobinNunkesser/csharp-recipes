efe64ee987333e12db1d7d46f696cd60e81e8957
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-02-18 21:55:22 +0100

    New recipe: MoneyExample

---
+++ MoneyExample/MoneyExample/Bank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyExample.Tests
+{
+    public class Bank
+    {
+        Dictionary<(string, string), int> rates = new Dictionary<(string, string), int>();
+
+        public Bank()
+        {
+        }
+
+        public Money Reduce(Money source, string to)
+        {
+            return source.Reduce(this, to);
+        }
+
+        public void AddRate(String from, String to, int rate)
+        {
+            rates[(from, to)] = rate;
+        }
+
+        public int? Rate(String from, String to)
+        {
+            if (from.Equals(to)) return 1;
+            return rates[(from, to)];
+        }
+    }
+}
