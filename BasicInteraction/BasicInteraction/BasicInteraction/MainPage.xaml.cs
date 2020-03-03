27a2ffca95e5c61f5ebb88b78dc3b5bab99c0787
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-03-03 07:33:48 +0100

    New recipe: BasicInteraction

---
+++ BasicInteraction/BasicInteraction/BasicInteraction/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BasicInteraction
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, EventArgs e)
+        {
+            TermLabel.Text = TermEntry.Text;
+        }
+    }
+}
