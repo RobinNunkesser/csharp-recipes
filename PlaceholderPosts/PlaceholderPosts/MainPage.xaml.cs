5cf314560ec293d7b18b87e483e383e693bac4f6
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-06-08 15:20:21 +0200

    Small Xamarin example

--- PlaceholderPosts/PlaceholderPosts/MainPage.xaml.cs
+++ PlaceholderPosts/PlaceholderPosts/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
+using PlaceholderPosts.Common;
+using PlaceholderPosts.Core;
+using PlaceholderPosts.Infrastructure;
 using Xamarin.Forms;
 
 namespace PlaceholderPosts
@@ -9,6 +12,9 @@ namespace PlaceholderPosts
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private readonly IAppService<PostRequest, PostEntity> _service =
+            new GetPostService(new PostRepositoryAdapter());
+
         public MainPage()
         {
             InitializeComponent();
@@ -16,6 +22,10 @@ namespace PlaceholderPosts
 
         private void Retrieve_Button_OnClicked(object sender, EventArgs e)
         {
+            _service.Execute(new PostRequest() {Id = int.Parse(IdEntry.Text)},
+                success => ResultLabel.Text = success.ToString(),
+                async error =>
+                    await DisplayAlert("Fehler", error.Message, "OK"));
         }
     }
 }
