9b19adc7595e14ea98140d32862cefb5b12b81da
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-09-04 13:01:52 +0200

    Reformatting

--- PlaceholderPosts/PlaceholderPosts.Core/PostEntity.cs
+++ PlaceholderPosts/PlaceholderPosts.Core/PostEntity.cs
@@ -3,11 +3,8 @@ namespace PlaceholderPosts.Core
     public class PostEntity
     {
         public int UserId { get; set; }
-
         public int Id { get; set; }
-
         public string Title { get; set; }
-
         public string Body { get; set; }
 
         public override string ToString() =>
--- PlaceholderPosts/PlaceholderPosts.Infrastructure/Post.cs
+++ PlaceholderPosts/PlaceholderPosts.Infrastructure/Post.cs
@@ -15,11 +15,8 @@ namespace PlaceholderPosts.Infrastructure
     public partial class Post
     {
         [JsonProperty("userId")] public long UserId { get; set; }
-
         [JsonProperty("id")] public long Id { get; set; }
-
         [JsonProperty("title")] public string Title { get; set; }
-
         [JsonProperty("body")] public string Body { get; set; }
     }
 
