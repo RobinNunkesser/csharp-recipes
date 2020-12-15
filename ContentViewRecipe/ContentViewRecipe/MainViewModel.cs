9062c1b881a6a702a72fe9dbe43247f93c2088ca
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-15 16:12:21 +0100

    New recipe

---
+++ ContentViewRecipe/ContentViewRecipe/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ContentViewRecipe
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string itemNr = "42";
+        public string ItemNr
+        {
+            get => itemNr;
+            set
+            {
+                if (itemNr != value)
+                {
+                    itemNr = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public MainViewModel()
+        {
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+          PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        #endregion
+
+    }
+}
