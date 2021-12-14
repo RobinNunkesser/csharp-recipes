fe43e4a413539f3daa494d2f7fa17b27414cdcd6
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-12-14 10:52:23 +0100

    New project

---
+++ HelloWorld/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
+using Application = Microsoft.Maui.Controls.Application;
+
+namespace HelloWorld
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+		
+		protected override void OnStart() { }
+		protected override void OnSleep() { }
+		protected override void OnResume() { }
+
+	}
+}
---
+++ HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace HelloWorld
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+		
+		protected override void OnAppearing() {}
+		protected override void OnDisappearing() {}        
+
+	}
+}
