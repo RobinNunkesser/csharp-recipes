17066d9aef2ff99acde6fed12e4219e2b7e67622
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-12-14 11:25:44 +0100

    Switching to MAUI

---
+++ BasicInteraction/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace BasicInteraction
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+			TermLabel.Text = TermEntry.Text.ToUpper();
+		}
+    }
+}
