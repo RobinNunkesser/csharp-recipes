fdd00c428fb4882f764ab76413d2ad78066384cc
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-12-14 14:10:54 +0100

    Switching to MAUI

---
+++ MVVMRecipe/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MVVMRecipe
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+			BindingContext = new MainViewModel();
+		}
+
+	}
+}
