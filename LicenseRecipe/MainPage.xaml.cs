53260412638bd93989411622557868e8bdba751e
Robin Nunkesser <robin.nunkesser@hshl.de>
2021-12-15 12:01:36 +0100

    Switch to MAUI

---
+++ LicenseRecipe/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace LicenseRecipe
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            var stream = await FileSystem.OpenAppPackageFileAsync("Resources/Raw/licenses.html");
+            string html = "";
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = html;
+            Browser.Source = htmlSource;
+        }
+
+    }
+}
