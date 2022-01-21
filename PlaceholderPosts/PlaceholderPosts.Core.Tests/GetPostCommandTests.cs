ef07f9edd52c5b70cfd20937ea97a445f402cb57
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-01-21 06:49:20 +0100

    Switch to MAUI

--- PlaceholderPosts/PlaceholderPosts.Core.Tests/GetPostCommandTests.cs
+++ PlaceholderPosts/PlaceholderPosts.Core.Tests/GetPostCommandTests.cs
@@ -1,7 +1,7 @@
 using Italbytz.Ports.Common;
 using Moq;
 using NUnit.Framework;
-using PlaceholderPosts.Common;
+using PlaceholderPosts.Core.Ports;
 
 namespace PlaceholderPosts.Core.Tests
 {
@@ -15,14 +15,27 @@ namespace PlaceholderPosts.Core.Tests
         [Test]
         public void TestExecute()
         {
-            var mock = new Mock<ICrudRepository<int, PostEntity>>();
-            var mockEntity = new PostEntity();
+            var mock = new Mock<ICrudRepository<long, IPost>>();
+            var mockEntity = new MockPost();
             mock.Setup(f => f.Retrieve(1))
                 .ReturnsAsync(mockEntity);
-            var service = new GetPostCommand(mock.Object);
-            service.Execute(new GetPostServiceDTO() { Id = 1 },
+            var service = new GetPostService(mock.Object);
+            service.Execute(new MockPostID() { Id = 1 },
                 success => Assert.AreEqual(mockEntity, success), Assert.Null);
             mock.Verify(f => f.Retrieve(1), Times.AtMostOnce());
         }
+    }
+
+    internal class MockPostID : IPostID
+    {
+        public long Id { get; set; }
+    }
+
+    internal class MockPost : IPost
+    {
+        public long UserId { get; set; }
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
     }
 }
