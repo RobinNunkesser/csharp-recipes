d2facda342d49cc7cc1d9d5d0ee74c23f95fbb1f
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-02-13 12:44:40 +0100

    Migrate to CollectionView

---
+++ TableRecipe/VerticalListPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TableRecipe
+{
+    public partial class VerticalListPage : ContentPage
+    {
+        public VerticalListPage()
+        {
+            InitializeComponent();
+            BindingContext = new VerticalListViewModel();
+        }
+    }
+}
