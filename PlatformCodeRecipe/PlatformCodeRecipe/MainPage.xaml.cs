fd074948d15d6984ba1ea755fed8c0f9b259791d
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-02-25 16:03:36 +0100

    New recipe

---
+++ PlatformCodeRecipe/PlatformCodeRecipe/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PlatformCodeRecipe.Services;
+
+namespace PlatformCodeRecipe;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		HelloWorldLabel.Text = (new HelloWorldService()).HelloWorld();
+		//SemanticScreenReader.Announce(HelloWorldLabel.Text);
+
+	}
+
+}
+
---
+++ PlatformCodeRecipe/PlatformCodeRecipe/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PlatformCodeRecipe;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
