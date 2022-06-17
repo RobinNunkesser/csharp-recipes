1ddef0c271ffe44c39e170ecd043ee5777317626
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-06-17 09:35:07 +0200

    MAUI Update

---
+++ NavigationRecipe/NavigationRecipe/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NavigationRecipe;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ NavigationRecipe/NavigationRecipe/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NavigationRecipe;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
