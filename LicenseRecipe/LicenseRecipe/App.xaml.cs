f5154813997a18e8c8e185673e7bc9e2f264e065
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-06-17 10:09:52 +0200

    MAUI Update

---
+++ LicenseRecipe/LicenseRecipe/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LicenseRecipe;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ LicenseRecipe/LicenseRecipe/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace LicenseRecipe
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly string Filename = "licenses.html";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            using var stream = await FileSystem.OpenAppPackageFileAsync(Filename);
+            using var reader = new StreamReader(stream);
+            var html = reader.ReadToEnd();
+
+            var htmlSource = new HtmlWebViewSource
+            {
+                Html = html
+            };
+            Browser.Source = htmlSource;
+        }
+
+    }
+}
