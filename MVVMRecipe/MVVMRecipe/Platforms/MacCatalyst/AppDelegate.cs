6c116c526e212719dad63b81e7735faee7d1058d
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-06-17 09:23:22 +0200

    MAUI Update

---
+++ MVVMRecipe/MVVMRecipe/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MVVMRecipe;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
