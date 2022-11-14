42567342556edcd425e698f1e5105ffda6bb245c
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-11-14 17:44:24 +0100

    New recipe

---
+++ AnimationRecipe/AnimationRecipe/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace AnimationRecipe;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await TextLabel.FadeTo(1, 1000);
+    }
+
+}
+
+
---
+++ AnimationRecipe/AnimationRecipe/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AnimationRecipe;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
