1a4d22e919af5d983307277592fa66216318e959
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-11-14 07:33:48 +0100

    New recipe

---
+++ MVURecipe/MVURecipe/MainState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MVURecipe
+{
+    public class MainState : BindingObject
+    {
+        public string Forename
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+        public string Surname
+        {
+            get => GetProperty<string>();
+            set => SetProperty(value);
+        }
+
+        internal void Reset()
+        {
+            Forename = "";
+            Surname = "";
+        }
+    }
+}
+
