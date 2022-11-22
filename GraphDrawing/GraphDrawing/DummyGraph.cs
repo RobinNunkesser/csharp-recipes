ef974e9d2353adba72b68e1fe83c56b6f4f15060
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-11-22 12:55:52 +0100

    Increment

---
+++ GraphDrawing/GraphDrawing/DummyGraph.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Msagl.Core.Geometry.Curves;
+using Microsoft.Msagl.Core.Layout;
+using Microsoft.Msagl;
+using Microsoft.Msagl.Miscellaneous;
+using Microsoft.Msagl.Prototype.Ranking;
+using Microsoft.Msagl.Layout.Layered;
+using Microsoft.Msagl.Core.Geometry;
+using Point = Microsoft.Msagl.Core.Geometry.Point;
+
+namespace GraphDrawing
+{
+    public static class DummyGraph
+    {
+        public static GeometryGraph Graph { get; set; } = new GeometryGraph();
+
+        static DummyGraph()
+        {
+            var settings = new SugiyamaLayoutSettings();
+
+            var nodeA = new Node(CurveFactory.CreateRectangle(1.0, 1.0, new Point()));
+            var nodeB = new Node(CurveFactory.CreateRectangle(1.0, 1.0, new Point()));
+
+            Graph.Nodes.Add(nodeA);
+            Graph.Nodes.Add(nodeB);
+
+            Graph.Edges.Add(new Edge(nodeA, nodeB)
+            {
+                Weight = 1
+            });
+
+            LayoutHelpers.CalculateLayout(Graph, settings, null);
+
+        }
+    }
+}
+
---
+++ GraphDrawing/GraphDrawing/GraphicsDrawable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Msagl.Drawing;
+
+namespace GraphDrawing
+{
+    public class GraphicsDrawable : IDrawable
+    {
+        public GraphicsDrawable()
+        {
+        }
+
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            foreach (var node in DummyGraph.Graph.Nodes)
+            {
+                System.Console.WriteLine($"{node.Center.X} {node.Center.Y} {node.BoundingBox.Left} {node.BoundingBox.Top}");
+
+
+            }
+
+            canvas.StrokeColor = Colors.Red;
+            canvas.StrokeSize = 6;
+            canvas.DrawLine(10, 10, 90, 100);
+
+        }
+    }
+}
+
